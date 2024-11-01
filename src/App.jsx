10663f073f78ae56a8e31363694f49620087a816
niraj-jha <jhaniraj45@gmail.com>
2024-11-01 23:16:34 +0530

    Finished understanding simple context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
+import ProfilePage from './components/Profile'
+import { ThemeProvider } from './contextsecond/themeContext'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [ themeMode, setThemeMode ] = useState("light");
+
+	const lightTheme = () => {
+		setThemeMode("light");
+	}
+	const darkTheme = () => {
+		setThemeMode("dark");
+	}
+
+	// actual change in theme:
+	useEffect(() => {
+		document.querySelector("html").classList.remove("dark", "light");
+		document.querySelector("html").classList.remove(themeMode);
+	}, [themeMode])
 
 	return (
-		<div>
-			Learning Context from Hitesh Chaudhary
-		</div>
+		<ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+			<div>
+				Learning Context from Hitesh Chaudhary
+				<Login />
+				<ProfilePage />
+			</div>
+			<div>
+				Theme Switcher
+			</div>
+		</ThemeProvider>
 	)
 }
 
